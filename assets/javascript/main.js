8a784f9e903fe3afcb0fbf8fe3abdbd8ba1dc61a
xhoang0509 <xhoang0509@gmail.com>
2021-07-27 23:23:49 +0700

    header navbar

---
+++ assets/javascript/main.js
@@ -0,0 +1,7 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+
+function addShadow() {
+    $('.header__search-input').classList.toggle('header__search-input-filed');
+}

b1859cb1a1292485fd72c40bb0767cebbee4a1b5
xhoang0509 <xhoang0509@gmail.com>
2021-07-28 22:27:47 +0700

    container slider

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,3 +5,23 @@ const $$ = document.querySelectorAll.bind(document)
 function addShadow() {
     $('.header__search-input').classList.toggle('header__search-input-filed');
 }
+
+var isDisplayCurrencyBox = false;
+function displayCurrencyBox() {
+    if (!isDisplayCurrencyBox) {
+        $('.currency__box').style.display = "flex";
+        isDisplayCurrencyBox = true;
+    }else {
+        $('.currency__box').style.display = "none";
+        isDisplayCurrencyBox = false;
+    }
+}
+
+function changeImageSlider1() {
+    $(".container__slider-img").src = "./assets/image/file (1).png";
+    $(".container__slider-img").classList.toggle("translateX");
+}
+function changeImageSlider2() {
+    $(".container__slider-img").src = "./assets/image/file.png";
+    $(".container__slider-img").classList.toggle("translateX");
+}
